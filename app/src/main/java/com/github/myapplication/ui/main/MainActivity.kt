8e0ec3156c0c8248114c464890e0aa1eb03a838a
firdaus1453 <mfirdaus1453@gmail.com>
2019-10-28 11:03:21 +0700

    Change position main

--- app/src/main/java/com/github/myapplication/ui/main/MainActivity.kt
+++ app/src/main/java/com/github/myapplication/ui/main/MainActivity.kt
@@ -1,14 +1,10 @@
-package com.github.myapplication
+package com.github.myapplication.ui.main
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.tabs.TabLayout
-import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-import com.github.myapplication.ui.main.SectionsPagerAdapter
+import androidx.viewpager.widget.ViewPager
+import com.github.myapplication.R
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
 
