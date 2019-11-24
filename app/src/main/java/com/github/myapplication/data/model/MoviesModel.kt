2aed0790f3fc43d73d78becfabd96c4277389f2f
firdaus1453 <mfirdaus1453@gmail.com>
2019-11-24 22:38:59 +0700

    Init submisson 1 : Add Poster Add Function to get data dummy Add Model for data dummy

---
+++ app/src/main/java/com/github/myapplication/data/model/MoviesModel.kt
@@ -0,0 +1,13 @@
+package com.github.myapplication.data.model
+
+/**
+ * Created by Muhammad Firdaus on 24/11/2019.
+ */
+data class MoviesModel(
+    val id: Int? = 0,
+    val title: String? = "",
+    val description: String? = "",
+    val image: Int? = 0,
+    val date: String? = "",
+    val rating: String? = ""
+)
